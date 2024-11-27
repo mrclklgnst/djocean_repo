055f265c56bfc8548b9f649acd8de4aede748d50
mrclklgnst <mkilgenstein@gmail.com>
2024-11-27 13:12:49 +0100

    added model output to index.html

--- oceanapp/views.py
+++ oceanapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Ocean
 
 def index(request):
-    return render(request, "oceanapp/index.html")
+    oceans = Ocean.objects.all()
+    con = {'ocean_data': oceans}
+    return render(request, "oceanapp/index.html", con)
 
 # Create your views here.
