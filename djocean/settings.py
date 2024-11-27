c90dfcf5a78d3f3ad15cd49727bb057dc1c073ca
mrclklgnst <mkilgenstein@gmail.com>
2024-11-27 17:26:33 +0100

    app runs on gunicorn connected to psql

--- djocean/settings.py
+++ djocean/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = "djocean.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ocean_db",
+        # "USER": "mkilall",
+        # "PASSWORD": "123",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
--- oceanapp/views.py
+++ oceanapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from .models import Ocean
+from .models import Ocean, Fish
 
 def index(request):
     oceans = Ocean.objects.all()
-    con = {'ocean_data': oceans}
+    fish = Fish.objects.all()
+    con = {'ocean_data': oceans,
+           'fish_data': fish}
     return render(request, "oceanapp/index.html", con)
 
 # Create your views here.
