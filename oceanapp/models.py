545efdf4e77fdbe6fbed5e3408b1f4fe34e9a9a4
mrclklgnst <mkilgenstein@gmail.com>
2024-11-27 12:45:56 +0100

    Added models.py updated gitignore

--- oceanapp/models.py
+++ oceanapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Ocean(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+class Fish(models.Model):
+    name = models.CharField(max_length=200)
+    ocean = models.ForeignKey(Ocean, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+class FishFact(models.Model):
+    fact = models.CharField(max_length=200)
+    fish = models.ForeignKey(Fish, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.fact
